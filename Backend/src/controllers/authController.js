3410f62952bf5c2c507decb0813dc9d5d32c1d36
Shuvam Biswal <shuvambiswal123@gmail.com>
2026-02-09 18:18:13 +0530

    Update authController.js

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -63,8 +63,10 @@ const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             //   secure: process.env.NODE_ENV === "production",
-            secure: false,
-            sameSite: "strict",
+            // secure: false,
+            secure: true,
+            // sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
